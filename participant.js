126e748193d7eda7df608e8189262f0e72f471e6
David Liedle <dliedle@cisco.com>
2021-07-15 17:02:27 -0600

    Formatting
    Adding semicolons to lines 3-8

--- participant.js
+++ participant.js
@@ -1,11 +1,11 @@
 var app = new window.Webex.Application();
 
-app.listen('application:displayContextChanged', (payload) => log('application:displayContextChanged', payload))
-app.listen('application:shareStateChanged', (payload) => log('application:shareStateChanged', payload))
-app.listen('application:themeChanged', (payload) => log('application:themeChanged', payload))
-app.listen('meeting:infoChanged', (payload) => log('meeting:infoChanged', payload))
-app.listen('meeting:roleChanged', (payload) => log('meeting:roleChanged', payload))
-app.listen('space:infoChanged', (payload) => log('space:infoChanged', payload))
+app.listen('application:displayContextChanged', (payload) => log('application:displayContextChanged', payload));
+app.listen('application:shareStateChanged', (payload) => log('application:shareStateChanged', payload));
+app.listen('application:themeChanged', (payload) => log('application:themeChanged', payload));
+app.listen('meeting:infoChanged', (payload) => log('meeting:infoChanged', payload));
+app.listen('meeting:roleChanged', (payload) => log('meeting:roleChanged', payload));
+app.listen('space:infoChanged', (payload) => log('space:infoChanged', payload));
 
 function handleGetUser(){
   app.context.getUser().then((u) => log('getUser()', u));
