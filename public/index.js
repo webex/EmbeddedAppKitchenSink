19dbef2b6aca4815f430cfa3cf2228212ea96c96
Adam Weeks <adweeks@cisco.com>
2022-11-28 16:57:21 -0500

    feat: add login with webex auth

--- public/index.js
+++ public/index.js
@@ -1,11 +1,12 @@
-const COOKIE_NAME = "webex_kitchen_sink_cookie";
-const LOCAL_STORAGE_NAME = "webex_kitchen_sink_localstorage";
-const SESSION_STORAGE_NAME = "webex_kitchen_sink_sessionstorage";
+// Check URL Hash for Login with Webex Token
+parseJwtFromURLHash();
 
-var app = new window.Webex.Application();
+const app = new window.Webex.Application();
 
 app.onReady().then(() => {
   log("onReady()", { message: "host app is ready" });
+
+  // Listen and emit any events from the EmbeddedAppSDK
   app.listen().then(() => {
     app.on("application:displayContextChanged", (payload) =>
       log("application:displayContextChanged", payload)
@@ -26,48 +27,10 @@ app.onReady().then(() => {
   });
 });
 
-function handleGetUser() {
-  app.context
-    .getUser()
-    .then((u) => {
-      log("getUser()", u);
-    })
-    .catch((error) => {
-      log(
-        "getUser() promise failed with error",
-        Webex.Application.ErrorCodes[error]
-      );
-    });
-}
-
-function handleGetMeeting() {
-  app.context
-    .getMeeting()
-    .then((m) => {
-      log("getMeeting()", m);
-    })
-    .catch((error) => {
-      log(
-        "getMeeting() promise failed with error",
-        Webex.Application.ErrorCodes[error]
-      );
-    });
-}
-
-function handleGetSpace() {
-  app.context
-    .getSpace()
-    .then((s) => {
-      log("getSpace()", s);
-    })
-    .catch((error) => {
-      log(
-        "getSpace() promise failed with error",
-        Webex.Application.ErrorCodes[error]
-      );
-    });
-}
-
+/**
+ * Sets the share url to the value entereed in the "shareUrl" element.
+ * @returns
+ */
 function handleSetShare() {
   if (app.isShared) {
     log("ERROR: setShareUrl() should not be called while session is active");
@@ -90,6 +53,9 @@ function handleSetShare() {
     });
 }
 
+/**
+ * Clears the share url
+ */
 function handleClearShare() {
   app
     .clearShareUrl()
@@ -102,121 +68,4 @@ function handleClearShare() {
         Webex.Application.ErrorCodes[error]
       );
     });
-}
-
-function handleDisplayAppInfo() {
-  log("Display Application", app);
-}
-
-function log(type, data) {
-  var ul = document.getElementById("console");
-  var li = document.createElement("li");
-  var payload = document.createTextNode(`${type}: ${JSON.stringify(data)}`);
-  li.appendChild(payload);
-  ul.prepend(li);
-}
-
-/** Cookies! **/
-
-function getCookieInputValue() {
-  let myValue = document.getElementById("cookieField").value;
-  return myValue;
-}
-
-function handleSetCookie() {
-  let myCookieValue = getCookieInputValue();
-  setCookie(COOKIE_NAME, myCookieValue, 365);
-  log("Cookie set!", myCookieValue);
-} // End handleSetCookie()
-
-function handleGetCookie() {
-  let myCookie = getCookie(COOKIE_NAME);
-  log("Cookie get! ", myCookie);
-} // End handleGetCookie()
-
-function handleClearCookie() {
-  document.cookie = `${COOKIE_NAME}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-  log("Cookie cleared!", document.cookie);
-} // End handleClearCookie()
-
-/** Source: https://www.w3schools.com/js/js_cookies.asp **/
-function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  let expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-  let name = cname + "=";
-  let ca = document.cookie.split(";");
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-
-/** localStorage! **/
-
-function handleSetLocalStorage() {
-  let myValue = document.getElementById("localField").value;
-  setLocalStorage(LOCAL_STORAGE_NAME, myValue);
-  log("localStorage set!", myValue);
-}
-function handleGetLocalStorage() {
-  let myValue = getLocalStorage(LOCAL_STORAGE_NAME);
-  log("localStorage get!", myValue);
-}
-
-function handleClearLocalStorage() {
-  deleteLocalStorageItem(LOCAL_STORAGE_NAME);
-  log("localStorage cleared!", getLocalStorage(LOCAL_STORAGE_NAME));
-}
-
-function deleteLocalStorageItem(key) {
-  window.localStorage.removeItem(key);
-}
-
-function getLocalStorage(keyName) {
-  const myStorage = window.localStorage.getItem(keyName);
-  return myStorage;
-}
-
-function setLocalStorage(key, value) {
-  window.localStorage.setItem(key, value);
-}
-
-/** sessionStorage! **/
-
-function handleSetSessionStorage() {
-  let myValue = document.getElementById("sessionField").value;
-  setSessionStorage(SESSION_STORAGE_NAME, myValue);
-  log("sessionStorage set!", myValue);
-}
-function handleGetSessionStorage() {
-  let myValue = getSessionStorage(SESSION_STORAGE_NAME);
-  log("sessionStorage get!", myValue);
-}
-
-function handleClearSessionStorage() {
-  deleteSessionStorageItem(SESSION_STORAGE_NAME);
-  log("sessionStorage cleared!", getSessionStorage(SESSION_STORAGE_NAME));
-}
-
-function deleteSessionStorageItem(key) {
-  window.sessionStorage.removeItem(key);
-}
-function getSessionStorage(keyName) {
-  const myStorage = window.sessionStorage.getItem(keyName);
-  return myStorage;
-}
-
-function setSessionStorage(key, value) {
-  window.sessionStorage.setItem(key, value);
 }
--- public/participant.js
+++ public/participant.js
@@ -1,3 +1,6 @@
+// Check URL Hash for Login with Webex Token
+parseJwtFromURLHash();
+
 var app = new window.Webex.Application();
 
 app.onReady().then(() => {
@@ -21,57 +24,3 @@ app.onReady().then(() => {
     app.on("space:infoChanged", (payload) => log("space:infoChanged", payload));
   });
 });
-
-function handleGetUser() {
-  app.context
-    .getUser()
-    .then((u) => {
-      log("getUser()", u);
-    })
-    .catch((error) => {
-      log(
-        "getUser() promise failed with error",
-        Webex.Application.ErrorCodes[error]
-      );
-    });
-}
-
-function handleGetMeeting() {
-  app.context
-    .getMeeting()
-    .then((m) => {
-      log("getMeeting()", m);
-    })
-    .catch((error) => {
-      log(
-        "getMeeting() promise failed with error",
-        Webex.Application.ErrorCodes[error]
-      );
-    });
-}
-
-function handleGetSpace() {
-  app.context
-    .getSpace()
-    .then((s) => {
-      log("getSpace()", s);
-    })
-    .catch((error) => {
-      log(
-        "getSpace() promise failed with error",
-        Webex.Application.ErrorCodes[error]
-      );
-    });
-}
-
-function handleDisplayAppInfo() {
-  log("Display Application", app);
-}
-
-function log(type, data) {
-  var ul = document.getElementById("console");
-  var li = document.createElement("li");
-  var payload = document.createTextNode(`${type}: ${JSON.stringify(data)}`);
-  li.appendChild(payload);
-  ul.prepend(li);
-}
